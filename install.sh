434ff2690889c7768d0f4a598d9cdabcb94e16cb
rxyvxr <109017615+rxyvxr@users.noreply.github.com>
2022-08-02 12:36:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+#  exit 1
+#fi
 
 # Updating System
-pacman -Syu
+sudo pacman -Syu
 
 # Making .config and Moving dotfiles and Background to .config
 mkdir ~/.config
@@ -16,15 +16,15 @@ mv ./dotconfig/* ~/.config
 mv ./bg.png ~/.config
 
 # Installing Essential Programs 
-pacman -S sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen
+sudo pacman -S sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen
 yay -S lxpolkit
 # Installing Other less important Programs
-pacman -S gimp vim lxappearance
+sudo pacman -S gimp vim lxappearance
 # Installing Custom ocs-url package
 #dnf install ./rpm-packages/ocs-url-3.1.0-1.fc20.x86_64.rpm
 
 # Installing fonts
-pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji
+sudo pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
